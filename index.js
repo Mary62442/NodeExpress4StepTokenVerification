e0c7e2ef42867863b17a3ee4960a1d1ebf372011
Maria Burlando <maria.bu62442@gmail.com>
2018-03-02 12:47:50 +0100

    Modified messages to be sent to the frontend

--- index.js
+++ index.js
@@ -22,7 +22,6 @@ let allowCrossDomain = function (req, res, next) {
     else { next(); }
 }
 
-
 app.use(allowCrossDomain);
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
@@ -30,8 +29,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/public/index.html')); 
-
-
 });
 
 app.post('/firststepsecurity', (req,res) => { 
@@ -42,7 +39,7 @@ app.post('/firststepsecurity', (req,res) => {
 
     let user = secret.secretData.users.find( user => user.name === username );
         if (typeof user === 'undefined') {
-            res.json({success: false, message:'Failure. User not found'}); 
+            res.json({success: false, message:'Failure. Invalid credentials'}); 
         };
     if (memorableA === user.memorableA) {
         if (firstPassword === user.firstPassword) {
@@ -51,21 +48,18 @@ app.post('/firststepsecurity', (req,res) => {
               });
               res.json({
                 success: true,
-                message: 'First JWT token created and signed!',
+                message: 'First JWT token created and signed by Captain America!',
                 username:user.name,
                 token: token,
                 secondRoute: secret.secretData.secondRoute
               });
         }
-        else res.json({success: false, message:'Failure. First JWT token not created'}); 
-    }  
-    
+        else res.json({success: false, message:'Failure. Invalid credentials'}); 
+    }      
     else {
-        res.json({success: false, message:'Failure. Incorrect memorable answer'});
-    }
-    
+        res.json({success: false, message:'Failure. Invalid credentials'});
+    }    
 });
-
 
 app.post('/tfrytiruytrrgr56767j98', (req,res) => {
     let secondPassword = req.get('custom-auth-step2');   
@@ -74,77 +68,69 @@ app.post('/tfrytiruytrrgr56767j98', (req,res) => {
 
     let user = secret.secretData.users.find( user => user.name === username );
         if (typeof user === 'undefined') {
-            res.json({success: false, message:'Failure. User not found'}); 
+            res.json({success: false, message:'Failure. Invalid credentials'}); 
         };
 
-    if (secondPassword === user.secondPassword) {
-        
+    if (secondPassword === user.secondPassword) {        
         if (token1) {
             // verifies secret and checks exp
             jwt.verify(token1, secret.secretData.tokenSignInSecretCaptain, (err, decoded) => {
                 if (err) {
-                return res.json({ success: false, message: 'Failed to authenticate token.' });
-                } 
-                
+                return res.json({ success: false, message: 'Failure. Invalid credentials' });
+                }                 
                 let token2 = jwt.sign(secret.secretData.signInEntityGeneral, secret.secretData.tokenSignInSecretGeneral, {
                     expiresIn: "2 days"
                     });
                 res.json({
                     success: true,
-                    message: 'Second JWT token created and signed!',
+                    message: 'Second JWT token created and signed by General Shang!',
                     token: token2,
                     username: user.name,
                     thirdRoute: secret.secretData.thirdRoute
-                });
-                
+                });                
             });        
         }
     }
     else {
-        res.json({success: false, message:'Failure. Second JWT token not created'});
+        res.json({success: false, message:'Failure. Invalid credentials'});
     }
-})
-
+});
 
 app.post('/dhaetyhaetgdhbfgbn5674ser', (req,res) => {
+
     let thirdPassword = req.get('custom-auth-step3');   
     let token2 = req.body.token2;
     let username = req.body.username;
 
     let user = secret.secretData.users.find( user => user.name === username );
         if (typeof user === 'undefined') {
-            res.json({success: false, message:'Failure. User not found'}); 
+            res.json({success: false, message:'Failure. Invalid credentials'}); 
         };
 
-    if (thirdPassword === user.thirdPassword) {
-        
+    if (thirdPassword === user.thirdPassword) {        
         if (token2) {
             // verifies secret and checks exp
             jwt.verify(token2, secret.secretData.tokenSignInSecretGeneral, (err, decoded) => {
                 if (err) {
-                    return res.json({ success: false, message: 'Failed to authenticate token.' });
-                } 
-                
+                    return res.json({ success: false, message: 'Failure. Invalid credentials' });
+                }                 
                 let token3 = jwt.sign(secret.secretData.signInEntityCommander, secret.secretData.tokenSignInSecretCommander, {
                     expiresIn: "2 days"
                     });
                 res.json({
-                success: true,
-                username: user.name,
-                message: 'Third JWT token created and signed!',
-                token: token3,
-                fourthRoute: secret.secretData.fourthRoute
-                });
-                
+                    success: true,
+                    username: user.name,
+                    message: 'Third JWT token created and signed by President Abraham Lincoln!',
+                    token: token3,
+                    fourthRoute: secret.secretData.fourthRoute
+                });                
             });        
         }
     }
     else {
-        res.json({success: false, message:'Failure. Third JWT token not created'});
+        res.json({success: false, message:'Failure. Invalid credentials'});
     } 
-
-})
-
+});
 
 app.post('/sdjogfnwo0aq23aojfnapw', (req,res) => {
     let fourthPassword = req.get('custom-auth-step4');   
@@ -153,43 +139,28 @@ app.post('/sdjogfnwo0aq23aojfnapw', (req,res) => {
 
     let user = secret.secretData.users.find( user => user.name === username );
         if (typeof user === 'undefined') {
-            res.json({success: false, message:'Failure. User not found'}); 
+            res.json({success: false, message:'Failure. Invalid credentials'}); 
         };
 
-    if (fourthPassword === user.fourthPassword) {
-        
+    if (fourthPassword === user.fourthPassword) {        
         if (token3) {
             // verifies secret and checks exp
             jwt.verify(token3, secret.secretData.tokenSignInSecretCommander, (err, decoded) => {
                 if (err) {
-                return res.json({ success: false, message: 'Failed to authenticate token.' });
-                } 
-                                    
+                return res.json({ success: false, message: 'Failure. Invalid credentials' });
+                }                                     
                 res.json({
                     success: true,
                     message: 'Success! You have accessed the most secret data!',
                     superSecretData:'Five busy honey bees were resting in the sun. The first one said, "Let us have some fun." The second one said, "Where shall it be?" The third one said, "In the honey tree." The fourth one said, "Let us make some honey sweet." The fifth one said, "With pollen on our feet." The five little busy bees sang their buzzing tune, As they worked in the beehive all that afternoon. Bzzzzzz! Bzzzzzz! Bzzzzzz! Bzzzzzz! Bzzzzzz!'
-                    
-                });
-                
-            });
-        
+                });                
+            });        
         }
-
     }
-
     else {
-        res.json({success: false, message:'Failure. You cannot access the most secret data'});
+        res.json({success: false, message:'Failure. Invalid credentials'});
     }
-
-    
-
-})
-
-
-
-
-
+});
 
 app.listen(port, ip);
 console.log('Server running on http://%s:%s', ip, port,"Process n." ,process.pid);
